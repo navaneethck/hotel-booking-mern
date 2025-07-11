fdb437b63650bf0a7fda1aef9260365e5f10df92
navaneeth ck <navaneethckn@gmail.com>
2025-07-11 12:14:23 +0530

    schema for booking updated

---
+++ models/bookingModel.js
@@ -0,0 +1,68 @@
+const mongoose= require('mongoose');
+
+const bookinSchema= new mongoose.Schema({
+
+user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User',
+    required:true
+},
+hotel:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Hotel',
+    required:true
+},
+checkInDate:{
+    type:Date,
+    required:true
+},
+checkOutDate:{
+    type:Date,
+    required:true
+},
+roomType:{
+    type:Number,
+    required:true,
+    min:1
+},
+  totalAmount: {
+    type: Number,
+    required: true
+  },
+  status:{
+    type:String,
+    enum:['pending','confirmed','cancelled','completed'],
+    default:'pending'
+  },
+    guests: {
+    adults: {
+      type: Number,
+      required: true,
+      min: 1
+    },
+    },
+    bookingReference: {
+    type: String,
+    unique: true
+  },
+    specialRequests: {
+    type: String,
+    maxLength: 500
+  },
+},{
+    timestamps:true
+  }
+);
+
+function generateBookingRef() {
+  return 'BK' + Math.floor(100000 + Math.random() * 900000);
+}
+
+bookinSchema.pre('save', function(next){
+    if(!this.bookingReference){
+         this.bookingReference = generateBookingRef();
+    }
+    next();
+})
+
+module.exports = mongoose.model('Booking', bookingSchema);
